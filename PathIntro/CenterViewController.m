4848d688aa4dd2779bd17fa8bbe7d5bc317e9994
JimmyHo <jimmyho@JimmyHotekiMacBook-Pro.local>
2013-11-06 23:21:57 +0800

    check point (download bug)

--- PathIntro/CenterViewController.m
+++ PathIntro/CenterViewController.m
@@ -67,6 +67,8 @@
     for (int i=0; i<20; i++) {
         [self loadScrollViewWithPage:i];
     }
+    
+    
 
 }
 
--- PathIntro/MovieViewController.m
+++ PathIntro/MovieViewController.m
@@ -28,11 +28,10 @@
     return self;
 }
 
--(UIImage*)getPoster:(int)page {
+-(void)getPoster:(int)page {
     
     NSString *posterName = [NSString stringWithFormat:@"%d.png",page];
     NSLog(@"%@",posterName);
-    __block UIImage *image;
     
     dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
     dispatch_async(queue, ^{
@@ -52,12 +51,10 @@
         dispatch_async(dispatch_get_main_queue(), ^{
             
             [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
-            image = [[UIImage alloc] initWithData:getObjectResponse.body];
             self.numberImage.image = [[UIImage alloc] initWithData:getObjectResponse.body];
             self.numberImageWithBlur.image = [self.numberImage.image applyLightEffect];
         });
     });
-    return image;
 }
 
 
